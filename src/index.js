ef8d831e2288aa03c1aa5074218a0c99b888f3c6
Nikita1814 <kravchenko1814@gmail.com>
2021-08-15 20:48:03 +0300

    feat: add number reversing function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let nStr = Math.abs(n).toString();
+    let reverseArr = [];
+    for (i = nStr.length; i >= 0; i--) {
+        reverseArr.push(nStr[i]);
+    }
+    return +reverseArr.join("");
+};
+
